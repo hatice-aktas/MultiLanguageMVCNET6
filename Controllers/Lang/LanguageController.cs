d5025a2e9d93a4e605f52214f2ce88b42e39ffaa
hatice-aktas <haticeaktas55.55@gmail.com>
2024-02-04 01:53:34 +0300

    - language dropdown changed (with image)

---
+++ Controllers/Lang/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+//using MultiLanguageMVC.Services.Lang;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace MultiLanguageMVC.Controllers.Lang
+{
+    public class LanguageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ChangeLanguage(string culture)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions() 
+                { 
+                    Expires = DateTimeOffset.UtcNow.AddYears(1) 
+                }
+            );
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+    }
+}
