41baf1cd696fe3030b594b49aa680dce3bcc46db
hatice-aktas <haticeaktas55.55@gmail.com>
2024-02-04 21:58:07 +0300

    - viewlocalizer kullanımı örneklendirmek için yeni bir sayfa oluşturuldu

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiLanguageMVCNET6.Controllers
+{
+    public class CountriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
